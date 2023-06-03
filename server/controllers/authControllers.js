f4e752f31dc6daab0d4f8d49c1956443fc97c990
jihad <jihad.zy@gmail.com>
2023-06-03 23:02:55 +0300

    chate reltime using node and react

---
+++ server/controllers/authControllers.js
@@ -0,0 +1,42 @@
+const User = require('../models/User');
+const jwt = require('jsonwebtoken');
+const maxAge = 5 * 24 * 60 * 60
+const createJWT = id => {
+    return jwt.sign({ id }, 'chatroom secret', {
+        expiresIn: maxAge
+    })
+}
+const alertError = (err) => {
+    let errors = { name: '', email: '', password: '' }
+
+    if (err.code === 11000) {
+        errors.email = 'This email already registered';
+        return errors;
+    }
+    if (err.message.includes('user validation failed')) {
+
+        Object.values(err.errors).forEach(({ properties }) => {
+            errors[properties.path] = properties.message
+        })
+    }
+    return errors
+}
+module.exports.signup = async (req, res) => {
+    const { name, email, password } = req.body;
+    try {
+        const user = await User.create({ name, email, password });
+        const token = createJWT(user._id);
+        res.cookie('jwt', token, { httpOnly: true, maxAge: maxAge * 1000 })
+        res.status(201).json({ user });
+    } catch (error) {
+        let errors = alertError(error);
+        res.status(400).json({ errors });
+    }
+
+}
+module.exports.login = (req, res) => {
+    res.send('login')
+}
+module.exports.logout = (req, res) => {
+    res.send('logout')
+}
